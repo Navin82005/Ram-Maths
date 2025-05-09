781d4e0d10f0fdec6ff138bd183cc957886d47f8
Navin82005 <navin82005@gmail.com>
2025-05-09 08:56:06 +0530

    frontend --init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Landing from "./page/Landing";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
